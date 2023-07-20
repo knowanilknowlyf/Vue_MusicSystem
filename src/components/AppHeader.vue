f5bbc59ebec5678401fbb8113ea4ea71ac4715b0
Anil Tiwari | V-Soft Consulting <atiwari@vsoftconsulting.com>
2023-07-20 13:28:27 +0530

    first commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link  class="text-white font-bold uppercase text-2xl mr-4" to="#">Music</router-link>
+      <router-link  class="text-white font-bold uppercase text-2xl mr-4" to="/" exact-active-class='no-active'>Music</router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
@@ -15,10 +15,10 @@
           </li>
           <template v-else>
             <li v-if="userStore.userLoggedIn">
-              <router-link  class="px-2 text-white" to="#" @click.prevent="userStore.signOut">Logout</router-link>
+              <a  class="px-2 text-white"  @click.prevent="userStore.signOut">Logout</a>
             </li>
             <li>
-              <router-link  class="px-2 text-white" to="#">Manage</router-link>
+              <router-link  class="px-2 text-white" to="/manage">Manage</router-link>
             </li>
           </template>
           <li><router-link  class="px-2 text-white" to="/about">about</router-link></li>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
 import About from '@/views/AboutView.vue'
+import Manage from '@/views/Manage.vue'
 
 const routes = [
   
@@ -15,12 +16,21 @@ const routes = [
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/manage',
+      component: Manage
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import('../views/AboutView.vue')
     }
   
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass:'text-yellow-500'
 })
 
 export default router
